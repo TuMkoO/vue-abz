238b6cd1ff7c7fda643d3c898d8682b6de72d570
TuMko <mr.t09@mail.ru>
2020-08-20 16:16:09 +0300

    Deploy

---
+++ precache-manifest.fb9e23fbc22d6b7f6706a8e0cfef6d1c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00daa2e8e9dabc5bd4e7",
+    "url": "/vue-abz/css/app.f2e768f8.css"
+  },
+  {
+    "revision": "cdddd16c56e1f51d014eb6c483aca3a7",
+    "url": "/vue-abz/favicon.png"
+  },
+  {
+    "revision": "f5443d74025f2a6069e0ca0142c08a05",
+    "url": "/vue-abz/img/banner-photo.f5443d74.jpg"
+  },
+  {
+    "revision": "146e29265bcc680129e263a9478dca00",
+    "url": "/vue-abz/img/logo.146e2926.svg"
+  },
+  {
+    "revision": "3cdf78971c869b16b95152be7b6922e5",
+    "url": "/vue-abz/img/man-laptop-v1.3cdf7897.svg"
+  },
+  {
+    "revision": "72f052f78efdeb4b8aa8f6e386b4bb0f",
+    "url": "/vue-abz/index.html"
+  },
+  {
+    "revision": "00daa2e8e9dabc5bd4e7",
+    "url": "/vue-abz/js/app.58f48be7.js"
+  },
+  {
+    "revision": "eba2d121d234c34f54e7",
+    "url": "/vue-abz/js/chunk-vendors.596eb3ff.js"
+  },
+  {
+    "revision": "56a6f3ee0919f6fdaa3f46e9cedfb96d",
+    "url": "/vue-abz/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-abz/robots.txt"
+  }
+]);
